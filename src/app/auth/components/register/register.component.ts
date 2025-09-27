1fc6c6d386e832f560c9d585873d798666469116
SofiiaHlushchak <glushchak02@gmail.com>
2025-09-27 12:07:31 +0200

    feat: register module

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+  standalone: false,
+})
+export class RegisterComponent {}
