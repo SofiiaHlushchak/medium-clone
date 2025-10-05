3e5a92be7c3012294decb072b1def919253d6e72
SofiiaHlushchak <glushchak02@gmail.com>
2025-10-05 12:19:01 +0200

    feat: feed module

---
+++ src/app/globalFeed/components/globalFeed.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mc-global-feed',
+    templateUrl: './globalFeed.component.html',
+    styleUrls: ['./globalFeed.component.scss'],
+    standalone: false,
+})
+export class GlobalFeedComponent {}
---
+++ src/app/globalFeed/globalFeed.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GlobalFeedComponent } from './components/globalFeed.component';
+import { RouterModule } from '@angular/router';
+import { FeedComponent } from '../shared/modules/feed/components/feed.component';
+
+const routes = [
+    {
+        path: '',
+        component: GlobalFeedComponent,
+    },
+];
+
+@NgModule({
+    imports: [CommonModule, RouterModule.forChild(routes)],
+    declarations: [GlobalFeedComponent, FeedComponent],
+    providers: [],
+})
+export class GlobalFeedModule {}
