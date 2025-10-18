d0c1c85cea956f3684449361c37f22835d38cca0
SofiiaHlushchak <glushchak02@gmail.com>
2025-10-18 11:44:44 +0200

    feat: create article module

--- src/app/createArticle/createArticle.module.ts
+++ src/app/createArticle/createArticle.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ArticleFormModule } from '../shared/modules/articleForm/articleForm.module';
 import { CreateArticleComponent } from './components/createArticle/createArticle.component';
+import { EffectsModule } from '@ngrx/effects';
+import { CreateArticleEffect } from './store/effects/createArticle.effect';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './store/reducers';
+import { CreateArticleService } from './services/createArticle.service';
 
 const routes = [
     {
@@ -12,7 +17,14 @@ const routes = [
 ];
 
 @NgModule({
-    imports: [CommonModule, RouterModule.forChild(routes), ArticleFormModule],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        ArticleFormModule,
+        EffectsModule.forFeature([CreateArticleEffect]),
+        StoreModule.forFeature('createArticle', reducers),
+    ],
     declarations: [CreateArticleComponent],
+    providers: [CreateArticleService],
 })
 export class CreateArticleModule {}
